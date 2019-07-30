ce220080376574f92d15c4b1178bfa1aa9bb72a9
Roma <user.sparkle@gmail.com>
2019-07-30 20:03:26 +0300

    finish desktop site

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,79 @@
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function () {
+    $('.owl-main').owlCarousel({
+        loop: true,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        items: 1,
+        autoplayTimeout: 10000
+    });
 
-	// Custom JS
+    $(function () {
+        var pull = $('.xs-menu');
+        menu = $('ul.menu-list');
+        menuHeight = menu.height();
+        $(pull).on('click', function (e) {
+            e.preventDefault();
+            menu.slideToggle();
+        });
 
+    });
+    $(window).resize(function () {
+        var w = $(window).width();
+        if (w > 320 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+
+
+    function Calendar2(id, year, month) {
+        var Dlast = new Date(year, month + 1, 0).getDate(),
+            D = new Date(year, month, Dlast),
+            DNlast = new Date(D.getFullYear(), D.getMonth(), Dlast).getDay(),
+            DNfirst = new Date(D.getFullYear(), D.getMonth(), 1).getDay(),
+            calendar = '<tr>',
+            month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        if (DNfirst != 0) {
+            for (var i = 1; i < DNfirst; i++) calendar += '<td>';
+        } else {
+            for (var i = 0; i < 6; i++) calendar += '<td>';
+        }
+        for (var i = 1; i <= Dlast; i++) {
+            if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
+                calendar += '<td class="today">' + i;
+            } else {
+                calendar += '<td>' + i;
+            }
+            if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {
+                calendar += '<tr>';
+            }
+        }
+        for (var i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
+        document.querySelector('#' + id + ' tbody').innerHTML = calendar;
+        document.querySelector('#' + id + ' thead td:nth-child(2)').innerHTML = month[D.getMonth()] + ' ' + D.getFullYear();
+        document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.month = D.getMonth();
+        document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.year = D.getFullYear();
+        if (document.querySelectorAll('#' + id + ' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
+            document.querySelector('#' + id + ' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
+        }
+    }
+
+    Calendar2("calendar2", new Date().getFullYear(), new Date().getMonth());
+// переключатель минус месяц
+    document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(1)').onclick = function () {
+        Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month) - 1);
+    }
+// переключатель плюс месяц
+    document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(3)').onclick = function () {
+        Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month) + 1);
+    }
+});
+
+$(document).on('click', '.dropdown-block', function () {
+    $(this).toggleClass('active-drop');
+    $(this).parent().toggleClass('active-drop');
+});
+
+$(document).on('click', '.close', function () {
+    $(this).parent('.dangers-item').remove()
 });
